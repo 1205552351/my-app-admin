6d4d648877d01822cf1208c6a45047de4a6d1140
shk <shk18031272429@163.com>
2021-02-24 23:51:27 +0800

    创建导航区与头部区域

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,8 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="less">
-body,html{
-  padding: 0;
-  margin: 0;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 </style>
